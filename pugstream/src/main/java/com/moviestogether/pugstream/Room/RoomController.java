a609565e41a8a0bf4aa61a7ca566fae31d289d30
Diana <ppkhloe@gmail.com>
2024-04-12 13:21:42 +0300

    Removed queue etc.

--- pugstream/src/main/java/com/moviestogether/pugstream/Room/RoomController.java
+++ pugstream/src/main/java/com/moviestogether/pugstream/Room/RoomController.java
@@ -1,11 +1,13 @@
 package com.moviestogether.pugstream.Room;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/room")
@@ -19,12 +21,19 @@ RoomRepository repository;
     }
     @PostMapping("/")
     public ResponseEntity AddRoom(@RequestBody Room room) {
+
         repository.save(room);
         return ResponseEntity.status(HttpStatus.OK).body(room);
     }
-//    public ResponseEntity DeleteRoom(@RequestBody Room room) {
-//        repository.delete(room);
-//    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity DeleteRoom(@PathVariable long id ) {
+        repository.findById(id);
+        Optional<Room> room=repository.findById(id);
+        if(room.isEmpty()){return ResponseEntity.status(HttpStatus.NOT_FOUND).body("String not found");}
+        repository.delete(room.get());
+        return ResponseEntity.status(HttpStatus.OK).body(room);
+
+    }
 
 }
 
