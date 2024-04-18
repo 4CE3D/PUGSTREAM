9e47eafec5ef4db7b3a256864f236c46662dfc03
Tamara Fatullaieva <wonderful0912@gmail.com>
2024-04-18 17:47:39 +0300

    Adding support for TikTok urls

--- pugstream/src/main/java/com/moviestogether/pugstream/Room/VideoIdParser.java
+++ pugstream/src/main/java/com/moviestogether/pugstream/Room/VideoIdParser.java
@@ -9,25 +9,33 @@ import java.util.regex.Pattern;
 public class VideoIdParser {
 
     public String extractId(String url) {
-        String embedCode = null;
+        String videoId, embedCode = null;
         if (url != null) {
-            if(url.contains(".mp4")){
+            if (url.contains(".mp4")) {
                 embedCode = "<iframe src=\"" + url + "\" frameborder=\"0\" allowfullscreen></iframe>";
             }
-            if(url.contains("youtube.com") || url.contains("youtu.be")) {
-                String videoId = extractYouTubeVideoId(url);
+            if (url.contains("youtube.com") || url.contains("youtu.be")) {
+                videoId = extractYouTubeVideoId(url);
                 // Need to be adjusted to media player
                 embedCode = "<iframe src=\"https://www.youtube.com/embed/" + videoId + "\" height=\"100%\" width=\"100%\" frameborder=\"0\" allowfullscreen></iframe>";
             }
-            if(url.contains("vimeo.com")) {
-                String videoId = extractVimeoVideoId(url);
+            if (url.contains("vimeo.com")) {
+                videoId = extractVimeoVideoId(url);
                 // Need to be adjusted to media player
                 embedCode = "<iframe src=\"https://player.vimeo.com/video/" + videoId + "\" height=\"100%\" width=\"100%\" frameborder=\"0\" allowfullscreen></iframe>";
             }
-            if(url.contains("twitch.tv")) {
-                String videoId = extractTwitchVideoId(url);
+            if (url.contains("twitch.tv")) {
+                videoId = extractTwitchVideoId(url);
                 // Need to be adjusted to media player. Need "parent" to be added
                 embedCode = "<iframe src=\"https://player.twitch.tv/?video=" + videoId + "&parent=\" height=\"100%\" width=\"100%\" frameborder=\"0\" allowfullscreen></iframe>";
+            }
+            if (url.contains("tiktok.com")) {
+                if (url.contains("vm.")) {
+                    System.out.println("Please, don't use app url"); // Needs to be changed
+                }
+                videoId = extractTiktokVideoId(url);
+                // Need to be adjusted to media player
+                embedCode = "<iframe src=\"https://www.tiktok.com/embed/v2/" + videoId + "\" height=\"100%\" width=\"100%\" frameborder=\"0\" allowfullscreen></iframe>";
             }
         } else {
             throw new IllegalStateException("No URL provided");
@@ -69,6 +77,19 @@ public class VideoIdParser {
             Matcher matcher = compiledPattern.matcher(url);
             if (matcher.find()) {
                 videoId = matcher.group();
+            }
+        }
+        return videoId;
+    }
+
+    private String extractTiktokVideoId(String url) {
+        String videoId = null;
+        if (url.contains("tiktok.com")) {
+            String pattern = "(?:tiktok\\.com\\/.*\\/|\\/)(\\d+)";
+            Pattern compiledPattern = Pattern.compile(pattern);
+            Matcher matcher = compiledPattern.matcher(url);
+            if (matcher.find()) {
+                videoId = matcher.group(1);
             }
         }
         return videoId;
